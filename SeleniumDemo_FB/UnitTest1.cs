711d4438d4f7d6933bb455bf68bc3e6e0baa02df
Soubarnika Muthu V <soubarnika.v@tvsnext.io>
2021-09-13 13:48:55 -0700

    [Soubarnika]Testing Facebook using selenium

---
+++ SeleniumDemo_FB/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SeleniumDemo_FB
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
